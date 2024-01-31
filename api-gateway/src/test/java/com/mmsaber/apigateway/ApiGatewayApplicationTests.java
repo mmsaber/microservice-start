7fd7c836d8c01024bef94e940439ca01521a9ffb
Mohamed Mohamed Saber <mm.saber91@gmail.com>
2024-01-31 21:30:10 +0300

    first commit

---
+++ api-gateway/src/test/java/com/mmsaber/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mmsaber.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
